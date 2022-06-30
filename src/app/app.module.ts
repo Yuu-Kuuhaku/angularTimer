72ef02d7cd061600ff8b2aec69bf1c5b62ed37b3
Yuu Kuuhaku <eusferalves@gmail.com>
2022-06-29 23:11:07 -0300

    Remoção de uma biblioteca que não foi usada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
 import { PageTimerComponent } from './page-timer/page-timer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 
 @NgModule({
   declarations: [
@@ -20,7 +18,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
