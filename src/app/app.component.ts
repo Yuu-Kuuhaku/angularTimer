d624af528da6c010326cb58a37d6aa360f525c7b
Eustaquio Ferreira <eusferalves@gmail.com>
2023-08-15 13:21:47 -0300

    mudanças no og meta, e mais imagens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent implements OnInit{
 
   urlBg = '';
 
-  arrayBg = ['./assets/1.jpg', './assets/3.jpg', './assets/4.jpg']
+  arrayBg = ['./assets/1.jpg', './assets/3.jpg', './assets/4.jpg', './assets/2.jpeg',  './assets/5.jpeg',  './assets/6.jpeg',  './assets/7.jpeg',  './assets/8.jpeg',  './assets/9.png',  './assets/10.jpeg']
 
   ngOnInit(): void {
     
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit{
 
   private radomNumber (){
     
-    return Math.floor(Math.random() * (2 + 1)) ;
+    return Math.floor(Math.random() * (9 + 1)) ;
 
   }
 }
